b26266e51a81f2b14029e4975889ed477b0cf021
Pavel <44953217+pewpewmeister@users.noreply.github.com>
2021-10-22 10:33:25 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const inner = document.querySelector('.inner')
+const squareColors = ['#ed220d', '#b542e5', '#0d93ed', '#f2770b', '#8aff8a', '#ff38c2', '#e37f35']
+const blocks = 672
+
+for (let i=0; i<blocks; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+  square.addEventListener('mouseover', () => setColor(square))
+  square.addEventListener('mouseout', () => removeColor(square))
+  inner.appendChild(square)
+}
+function setColor(elem) {
+  const color = getRandomColor()
+  elem.style.background = color
+  elem.style.boxShadow = `0 0 12px ${color}`
+}
+function removeColor(elem) {
+  elem.style.background = '#2d2d2d'
+  elem.style.boxShadow = '0 0 4px #000'
+}
+const getRandomColor = () => squareColors[Math.floor(Math.random() * squareColors.length)]
